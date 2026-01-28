40a4776910a42345046877a8b918d23652d0fff3
Tandith 0005 <tandith0005@gmail.com>
2026-01-28 16:08:59 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Application } from "express";
+import cors from 'cors';
+import { notFound } from "./middleware/notFound";
+import { auth } from "./lib/auth";
+import { toNodeHandler } from "better-auth/node";
+const app: Application = express()
+
+
+app.use(cors({
+  origin: process.env.APP_URL || "http://localhost:3000", // client side url
+  credentials: true
+}));
+app.use(express.json());
+
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+
+app.use(notFound);
+
+
+export default app;
+
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import { prisma } from "./lib/prisma";
+import app from "./app";
+const port = 5000;
+
+async function server() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully.");
+
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+
+  } catch (e) {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+server();
