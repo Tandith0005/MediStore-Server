bb05bf2579e333baf0d3fed10899372338273a9d
Tandith 0005 <tandith0005@gmail.com>
2026-02-03 12:28:46 +0600

    vercel will fix indeed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,22 @@
-import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma";
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
+const connectionString = process.env.DATABASE_URL!;
 
-const connectionString = `${process.env.DATABASE_URL}`
- 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const adapter = new PrismaPg({ connectionString });
 
-export { prisma }
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
