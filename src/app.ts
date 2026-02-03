914246bc4c5ad86a2c9aa77c9ad6e1f82338cb01
Tandith 0005 <tandith0005@gmail.com>
2026-02-03 12:37:10 +0600

    changed main app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,28 +10,38 @@ import { userRoutes } from "./modules/user/user.routes";
 import { dashboardRouter } from "./modules/dashboard/dashboard.route";
 import { orderRouter } from "./modules/orders/order.routes";
 import { categoryRouter } from "./modules/category/category.routes";
-const app: Application = express()
+const app: Application = express();
 
+/* =======================
+   Middlewares
+======================= */
+app.use(
+  cors({
+    origin: [process.env.APP_URL || "http://localhost:3000"],
+    credentials: true,
+  })
+);
 
-app.use(cors({
-  origin: [process.env.APP_URL || "http://localhost:3000"], // client side url
-  credentials: true
-}));
 app.use(express.json());
 
-
-app.all('/api/auth/{*any}', toNodeHandler(auth));
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+/* =======================
+   Health check (IMPORTANT)
+======================= */
+app.get("/api", (req, res) => {
+  res.json({ message: "API is running successfully" });
 });
 
-app.use('/shop', shopRouter);
-app.use('/cart', cartRouter);
-app.use('/medicine', medicineRouter);
-app.use('/user', userRoutes);
-app.use('/adminDashboard-stats', dashboardRouter);
-app.use("/orders", orderRouter);
-app.use("/categories", categoryRouter);
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+/* =======================
+   API Routes
+======================= */
+app.use("/api/shop", shopRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/medicine", medicineRouter);
+app.use("/api/user", userRoutes);
+app.use("/api/adminDashboard-stats", dashboardRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/categories", categoryRouter);
 
 
 app.use(notFound);
