7914402e9d05b67281042277cad178b4a26dac71
Tandith 0005 <tandith0005@gmail.com>
2026-02-03 12:39:35 +0600

    updated to previous

--- src/app.ts
+++ src/app.ts
@@ -10,38 +10,28 @@ import { userRoutes } from "./modules/user/user.routes";
 import { dashboardRouter } from "./modules/dashboard/dashboard.route";
 import { orderRouter } from "./modules/orders/order.routes";
 import { categoryRouter } from "./modules/category/category.routes";
-const app: Application = express();
+const app: Application = express()
 
-/* =======================
-   Middlewares
-======================= */
-app.use(
-  cors({
-    origin: [process.env.APP_URL || "http://localhost:3000"],
-    credentials: true,
-  })
-);
 
+app.use(cors({
+  origin: [process.env.APP_URL || "http://localhost:3000"], // client side url
+  credentials: true
+}));
 app.use(express.json());
 
-/* =======================
-   Health check (IMPORTANT)
-======================= */
-app.get("/api", (req, res) => {
-  res.json({ message: "API is running successfully" });
-});
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
-/* =======================
-   API Routes
-======================= */
-app.use("/api/shop", shopRouter);
-app.use("/api/cart", cartRouter);
-app.use("/api/medicine", medicineRouter);
-app.use("/api/user", userRoutes);
-app.use("/api/adminDashboard-stats", dashboardRouter);
-app.use("/api/orders", orderRouter);
-app.use("/api/categories", categoryRouter);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.use('/shop', shopRouter);
+app.use('/cart', cartRouter);
+app.use('/medicine', medicineRouter);
+app.use('/user', userRoutes);
+app.use('/adminDashboard-stats', dashboardRouter);
+app.use("/orders", orderRouter);
+app.use("/categories", categoryRouter);
 
 
 app.use(notFound);
