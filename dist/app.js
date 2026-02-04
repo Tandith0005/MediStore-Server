894d0a8074b5db49e1a7d090d641dab256004416
udayhasan <udayhasan15@gmail.com>
2026-02-04 23:24:03 +0600

    changes

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import { notFound } from "./middleware/notFound";
 import { auth } from "./lib/auth";
 import { toNodeHandler } from "better-auth/node";
@@ -10,21 +10,23 @@ import { userRoutes } from "./modules/user/user.routes";
 import { dashboardRouter } from "./modules/dashboard/dashboard.route";
 import { orderRouter } from "./modules/orders/order.routes";
 import { categoryRouter } from "./modules/category/category.routes";
+import { config } from "dotenv";
 const app = express();
+config();
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 app.use(cors({
     origin: [process.env.APP_URL || "http://localhost:3000"], // client side url
-    credentials: true
+    credentials: true,
 }));
 app.use(express.json());
-app.all('/api/auth/{*any}', toNodeHandler(auth));
-app.get('/', (req, res) => {
-    res.send('Medi Store Server Running!');
+app.get("/", (req, res) => {
+    res.send("Medi Store Server Running!");
 });
-app.use('/api/shop', shopRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/medicine', medicineRouter);
-app.use('/api/user', userRoutes);
-app.use('/api/adminDashboard-stats', dashboardRouter);
+app.use("/api/shop", shopRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/medicine", medicineRouter);
+app.use("/api/user", userRoutes);
+app.use("/api/adminDashboard-stats", dashboardRouter);
 app.use("/api/orders", orderRouter);
 app.use("/api/categories", categoryRouter);
 app.use(notFound);
