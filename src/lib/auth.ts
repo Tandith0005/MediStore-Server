51cf24582db1a75390377516f65771e340f6b3be
Tandith 0005 <tandith0005@gmail.com>
2026-02-06 16:24:21 +0600

    cors issue

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,33 +2,46 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma.js";
 
-
-
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", 
-    }),
-    trustedOrigins: [process.env.APP_URL!, "http://localhost:5000"],
-    user: {
-        additionalFields: {
-            role: {
-                type: "string",
-                defaultValue: "CUSTOMER",
-                required: false
-            },
-        }
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  trustedOrigins: [process.env.APP_URL!, "http://localhost:5000"],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "CUSTOMER",
+        required: false,
+      },
     },
-    
-    emailAndPassword: { 
-    enabled: true, 
-    }, 
+  },
 
+  emailAndPassword: {
+    enabled: true,
+  },
 
-    socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID as string, 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
-        }, 
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
-    //  Logout is implemented on the client side
+  },
+
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // 5 minutes
+    },
+  },
+  advanced: {
+    cookiePrefix: "better-auth",
+    useSecureCookies: process.env.NODE_ENV === "production",
+    crossSubDomainCookies: {
+      enabled: false,
+    },
+    disableCSRFCheck: true, // Allow requests without Origin header (Postman, mobile apps, etc.)
+  },
+
+  //  Logout is implemented on the client side
 });
